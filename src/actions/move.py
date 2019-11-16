59f058ff269f6e65529aaf5bd8c7cabbca549ecf
dolu-bl <dolu.bl@gmail.com>
2019-11-16 15:45:15 +0300

    Mouse click moving added.

--- src/actions/move.py
+++ src/actions/move.py
@@ -10,6 +10,9 @@ class Move(Action):
     def __init__(self, game):
         super().__init__(game)
         self.direction = None
+        surface = pygame.display.get_surface()
+        self.windowWidth = surface.get_width()
+        self.windowHeight = surface.get_height()
 
     def process(self):
         x = self.board.playerPosition[0]
@@ -24,19 +27,35 @@ class Move(Action):
             self.tryMove(x - 1, y)
 
     def handle(self, event):
-        if event.type != pygame.KEYUP:
-            return False
+        if event.type == pygame.KEYUP:
+            self.direction = None
+            if event.key == pygame.K_UP:
+                self.direction = Direction.Up
+            elif event.key == pygame.K_RIGHT:
+                self.direction = Direction.Right
+            elif event.key == pygame.K_DOWN:
+                self.direction = Direction.Down
+            elif event.key == pygame.K_LEFT:
+                self.direction = Direction.Left
+            return self.direction != None
 
-        self.direction = None
-        if event.key == pygame.K_UP:
-            self.direction = Direction.Up
-        elif event.key == pygame.K_RIGHT:
-            self.direction = Direction.Right
-        elif event.key == pygame.K_DOWN:
-            self.direction = Direction.Down
-        elif event.key == pygame.K_LEFT:
-            self.direction = Direction.Left
-        return self.direction != None
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            self.direction = None
+            x = event.pos[0] - self.windowWidth / 2
+            y = event.pos[1] - self.windowHeight / 2
+            if x > y:
+                if x > -y:
+                    self.direction = Direction.Right
+                else:
+                    self.direction = Direction.Up
+            else:
+                if x > -y:
+                    self.direction = Direction.Down
+                else:
+                    self.direction = Direction.Left
+            return self.direction != None
+
+        return False
 
     def tryMove(self, x, y):
         if x < 0 : x = self.board.width - 1;
